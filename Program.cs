9001b8785ad029f41a0367c5f40c9b418b63abdc
Leandro Santana <96756735+Leandro-Santana013@users.noreply.github.com>
2025-03-17 01:51:30 -0300

    exforca

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string palavra = "palmeiras";
+Console.Write("Adivinhe uma letra: ");
+string chute = Console.ReadLine();
+
+Console.Write((palavra.IndexOf(chute) >= 0) ? $"A letra {chute} está presente na palavra!" : " Errou!");
